0bcdcdced7f03fbca1ff4a422b4ca3c96e2fd292
allfigueiredodev <allfigueiredo@outlook.com>
2023-10-02 13:18:36 -0300

    lst_utils update

--- lst_utils.c
+++ lst_utils.c
@@ -6,25 +6,26 @@
 /*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 18:33:49 by aperis-p          #+#    #+#             */
-/*   Updated: 2023/09/29 22:00:17 by aperis-p         ###   ########.fr       */
+/*   Updated: 2023/10/01 01:56:48 by aperis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void print_dlist(t_dclist *stack)
-{
-	t_dclist *temp;
-	temp = stack;
-	stack = stack->next;
-	ft_printf("%d ", temp->content);
-	while(stack->next != temp)
-	{
-		ft_printf("%d ", stack->content);
-		stack = stack->next;
-	}
-	ft_printf("\n");
-}
+// void print_dlist(t_dclist *stack)
+// {
+// 	t_dclist *temp;
+// 	temp = stack;
+// 	stack = stack->next;
+// 	ft_printf("%d ", temp->index);
+// 	while(stack->next != temp)
+// 	{
+// 		ft_printf("%d ", stack->index);
+// 		stack = stack->next;
+// 	}
+// 	ft_printf("%d ", stack->index);
+// 	ft_printf("\n");
+// }
 
 int	dc_lstsize(t_dclist *lst)
 {
@@ -34,13 +35,14 @@ int	dc_lstsize(t_dclist *lst)
 	total = 0;
 	temp = lst;
 	lst = lst->next;
-	if(lst == temp)
+	if(lst == temp || lst->next == NULL)
 		return (0);
 	while (lst != temp)
 	{
 		total++;
 		lst = lst->next;
 	}
+	total++;
 	return (total);
 }
 
@@ -56,45 +58,59 @@ t_dclist	*dc_lst_last(t_dclist *lst)
 	lst = lst->next;
 	while (lst != temp)
 		lst = lst->next;
+	ft_printf("%d\n", lst->index);
 	return (lst);
 }
 
-void	lst_prev_next(t_dclist **alst, t_dclist *new)
-{
-	t_dclist	*last;
-	t_dclist	*head;
+// void	lst_prev_next(t_dclist **alst, t_dclist *new)
+// {
+// 	t_dclist	*last;
+// 	t_dclist	*head;
 
-	head = *(alst);
-	if (alst)
-	{
-		if (*alst == NULL)
-			*alst = new;
-		else
-		{
-			last = ft_lstlast(*(alst));
-			new = last;
-			last->next = head;
-		}
-	}
-	print_list(*alst);
-}
+// 	head = *(alst);
+// 	if (alst)
+// 	{
+// 		if (*alst == NULL)
+// 			*alst = new;
+// 		else
+// 		{
+// 			last = ft_lstlast(*(alst));
+// 			new = last;
+// 			last->next = head;
+// 		}
+// 	}
+// 	print_list(*alst);
+// }
 
-t_dclist	*lst_new_node(int content)
-{
-	t_dclist	*node;
+// t_dclist	*lst_new_node(int content)
+// {
+// 	t_dclist	*node;
 
-	node = (t_dclist *)malloc(sizeof(t_dclist));
-	if (!node)
-		return (NULL);
-	node->content = content;
-	node->prev = node;
-	node->next = node;
-	return (&*node);
-}
+// 	node = (t_dclist *)malloc(sizeof(t_dclist));
+// 	if (!node)
+// 		return (NULL);
+// 	node->content = content;
+// 	node->prev = node;
+// 	node->next = node;
+// 	return (&*node);
+// }
 
 int main(void)
 {
-	int nbr = 5;
-
-	print_dlist(lst_new_node(nbr));
+	t_dclist node1;
+	t_dclist node2;
+	t_dclist node3;
+	
+	node1.index = 0;
+	node2.index = 1;
+	node3.index = 2;
+	node1.next = &node1;
+	node1.prev = &node3;
+	node2.next = &node3;
+	node2.prev = &node1;
+	node3.next = &node1;
+	node3.prev = &node2;	
+	// print_dlist(&node1);
+	// ft_printf("%d\n", dc_lstsize(&node1));
+	dc_lst_last(&node1);
 }
